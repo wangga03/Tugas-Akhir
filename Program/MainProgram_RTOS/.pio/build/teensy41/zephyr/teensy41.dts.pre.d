c479fe64da59f3df34376a3f9ad4d6def362dc08
wangga03 <aswanggasadu@gmail.com>
2025-10-22 16:17:41 +0700

    menambahkan projek KiCad

---
+++ Program/MainProgram_RTOS/.pio/build/teensy41/zephyr/teensy41.dts.pre.d
@@ -0,0 +1,12 @@
+empty_file.o: \
+ /home/wgg/.platformio/packages/framework-zephyr/misc/empty_file.c \
+ /home/wgg/.platformio/packages/framework-zephyr/boards/arm/teensy4/teensy41.dts \
+ /home/wgg/.platformio/packages/framework-zephyr/boards/arm/teensy4/teensy40.dts \
+ /home/wgg/.platformio/packages/framework-zephyr/dts/arm/nxp/nxp_rt1060.dtsi \
+ /home/wgg/.platformio/packages/framework-zephyr/dts/arm/nxp/nxp_rt.dtsi \
+ /home/wgg/.platformio/packages/framework-zephyr/dts/common/mem.h \
+ /home/wgg/.platformio/packages/framework-zephyr/dts/arm/armv7-m.dtsi \
+ /home/wgg/.platformio/packages/framework-zephyr/dts/common/skeleton.dtsi \
+ /home/wgg/.platformio/packages/framework-zephyr/include/dt-bindings/clock/imx_ccm.h \
+ /home/wgg/.platformio/packages/framework-zephyr/include/dt-bindings/gpio/gpio.h \
+ /home/wgg/.platformio/packages/framework-zephyr/include/dt-bindings/i2c/i2c.h
